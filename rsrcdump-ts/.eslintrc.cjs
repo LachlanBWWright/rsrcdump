b393c792da20ee13408e6fd67476f777c931f454
LachlanBWWright <lachlanbwwright@gmail.com>
2025-12-27 22:02:24 +1100

    Npm publish work, update readme

--- rsrcdump-ts/.eslintrc.cjs
+++ rsrcdump-ts/.eslintrc.cjs
@@ -6,20 +6,23 @@ module.exports = {
     sourceType: "module",
     project: ["./tsconfig.json"],
   },
-  plugins: ['@typescript-eslint', 'vitest'],
+  plugins: ["@typescript-eslint", "vitest"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vitest/recommended',
-    'prettier'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vitest/recommended",
+    "prettier",
   ],
   env: {
     node: true,
-    es2022: true
+    es2022: true,
   },
   rules: {
     "no-console": "warn",
-    "@typescript-eslint/no-unused-vars": ["error", { "varsIgnorePattern": "^_", "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+    ],
     "@typescript-eslint/no-explicit-any": "off",
   },
   ignorePatterns: ["build/", "dist/", "node_modules/", "*.d.ts"],
